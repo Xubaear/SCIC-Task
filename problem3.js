4bb865131e6b039f9da56d7dc7808f8245baaa17
Xubaear <zubaear.hasan7@gmail.com>
2026-01-07 23:33:06 +0600

    problem 3

---
+++ problem3.js
@@ -0,0 +1,13 @@
+function isPalindrome (string){
+    const reverseSame = string.split('').reverse('').join('')
+
+    if (string === reverseSame){
+        return true;
+
+    }else{
+        return false
+    }
+}
+
+console.log(isPalindrome ("madam"))
+console.log(isPalindrome ('hello'))
