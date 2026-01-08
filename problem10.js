32e4af34ad863a9d2a9b9d30ffb09e1f30f7c52c
Xubaear <zubaear.hasan7@gmail.com>
2026-01-08 13:06:48 +0600

    problem10

---
+++ problem10.js
@@ -0,0 +1,20 @@
+function pingPong(){
+    for (let x =1 ;
+        x<=20;
+        x++
+    ){
+        if(x % 3 ===0){
+            console.log('Ping')
+        }
+        else if(x % 5 ===0){
+            console.log('Pong')
+        }
+        else if (x % 3 && x % 5 === 0){
+            console.log('PingPong')
+        }
+        else{
+            console.log(x)
+        }
+    }
+}
+pingPong()
